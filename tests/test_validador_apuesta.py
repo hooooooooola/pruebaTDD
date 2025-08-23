d8998fa63b074d61240c0f51a6f5f625e90647a3
pipe <ftilleria2023@udec.clm>
2025-08-23 19:10:28 -0400

    First Commit

---
+++ tests/test_validador_apuesta.py
@@ -0,0 +1,7 @@
+from src.juego.validador.apuesta import ValidadorApuesta
+
+def test_validador_apuesta():
+    validador = ValidadorApuesta()
+    assert validador.validar_apuesta(100) == True
+    assert validador.validar_apuesta(0) == False
+    assert validador.validar_apuesta(-50) == False
