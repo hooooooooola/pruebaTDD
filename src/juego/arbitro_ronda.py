116b5c23fe43b63cfddceb356baa2a8c74961243
hooooooooola <167253637+hooooooooola@users.noreply.github.com>
2025-08-26 19:46:50 +0000

    GREEN: implementación mínima para validar un calce

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -27,3 +27,16 @@ class ArbitroRonda:
         else:
             cacho_calzador.perder_dado()
             return 0
+
+    def puede_calzar(self, cacho_calzador, cachos):
+        """
+        puede calzar si: 
+        - si tiene solo un dado
+        - si hay la mitad o mas de dados iniciales en juego
+        """
+        # un dado
+        if cacho_calzador.cantidad_dados() == 1:
+            return True
+        
+        # mitad de los dados (dados actuales >= cachos*5 / 2)
+        return sum(cacho.cantidad_dados() for cacho in cachos) >= len(cachos) * 5 / 2
