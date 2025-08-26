c9d758a26ba53b884ebcca0a726f17bca490b368
hooooooooola <167253637+hooooooooola@users.noreply.github.com>
2025-08-26 15:17:54 -0400

    Delete tests directory

--- tests/test_validador_apuesta.py
+++
@@ -1,6 +0,0 @@
-from src.juego.validador.apuesta import ValidadorApuesta
-
-def test_validador_apuesta():
-    validador = ValidadorApuesta()
-    assert validador.validar_apuesta(100) == True
-    assert validador.validar_apuesta(0) == False
