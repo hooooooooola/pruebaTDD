610a65823a136b6572402d5171429e13a608a5a5
hooooooooola <167253637+hooooooooola@users.noreply.github.com>
2025-08-27 01:08:51 +0000

    RED: test para cacho inicial - falla como esperado

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -1,4 +1,7 @@
+import pytest
+from unittest.mock import Mock, patch
 from src.juego.gestor_partida import GestorPartida
+
 
 def test_crear_partida_con_cachos():
     """
@@ -11,6 +14,40 @@ def test_crear_partida_con_cachos():
     assert len(gestor.cachos) == 3
     for cacho in gestor.cachos:
         assert cacho.cantidad_dados() == 5
-    assert gestor.cacho_actual == None
     assert gestor.cacho_actual is None
     assert gestor.direccion is None
+
+
+def test_determinar_cacho_inicial_sin_empate(mocker):
+    """
+    el cacho con el dado más alto inicia la partida
+    """
+    gestor = GestorPartida(3)
+    
+    # mocks para no usar testear con randoms 
+    mock_dados = [3, 6, 4]  
+    
+    with patch('src.juego.gestor_partida.GeneradorAleatorio.generar_numero', side_effect=mock_dados): # use esto with patch con un mock para reemplazar los numeros random, no se si hay una mejor manera pero lo dejo asi por mientras :D
+        # act
+        cacho_inicial = gestor.determinar_cacho_inicial()
+    
+    # assert
+    assert cacho_inicial == gestor.cacho_actual
+
+
+def test_determinar_cacho_inicial_con_empate(mocker):
+    """
+    en caso de empate se vuelve a tirar dados solo para los empatados
+    """
+    # Arrange
+    gestor = GestorPartida(2)
+    
+    # mock para randoms
+    mock_dados = [5, 5, 2, 6] 
+    
+    with patch('src.juego.gestor_partida.GeneradorAleatorio.generar_numero', side_effect=mock_dados): # lo mismo por aca
+        # act
+        cacho_inicial = gestor.determinar_cacho_inicial()
+    
+    # assert
+    assert cacho_inicial == gestor.cacho_actual
